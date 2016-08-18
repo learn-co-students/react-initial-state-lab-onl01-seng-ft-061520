f4e45777095d44a9e0c63078604852e4adc65211
thomastuts <thomas@thomastuts.com>
2016-08-18 16:42:54 +0200

    Scaffold files for new testing strategy

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const app = "I don't do much.";
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const ImageSlider = require('./components/ImageSlider');
+const BombTimer = require('./components/BombTimer');
+
+ReactDOM.render(
+  <div>
+    <ImageSlider></ImageSlider>
+    <BombTimer initialTime={120} />
+  </div>,
+  document.getElementById('main')
+);
