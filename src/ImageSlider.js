b59801e6faab326353bbe247006059e45ec8ea66
Luke Ghenco <lukeghenco@gmail.com>
2017-06-05 12:18:29 -0400

    Add solution code

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,20 @@
-// your ImageSlider code here!
+import React from 'react';
+
+class ImageSlider extends React.Component {
+
+  constructor() {
+    super() 
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    return (
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    )
+  }
+}
+
+export default ImageSlider;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ImageSlider from './ImageSlider';
 import Bomb from './Bomb';
 
 ReactDOM.render(
-  <div>
-    <ImageSlider />
-    <Bomb initialCount={120} />
-  </div>,
-  document.getElementById('root')
+	<div>
+		<ImageSlider />
+		<Bomb initialCount={120} />
+	</div>,
+	document.getElementById('root')
 );
