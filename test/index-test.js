7887f475e2757f5ba7b9045068cb1e31e00bd91b
Luke Ghenco <lukeghenco@gmail.com>
2017-06-05 12:16:27 -0400

    Update tests to use Chai

--- test/index-test.js
+++ test/index-test.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import ImageSlider from '../components/ImageSlider';
-import Bomb from '../components/Bomb';
+import ImageSlider from '../src/components/ImageSlider';
+import Bomb from '../src/components/Bomb';
 
 describe('<ImageSlider />', function () {
   const wrapper = shallow(<ImageSlider/>);
 
   it('should set the `currentSlideIndex` to 0 on its state', function () {
-    expect(wrapper.state('currentSlideIndex'))
-      .toNotBe(undefined, 'The `currentSlideIndex` property is not defined on the state.')
-      .toEqual(0);
+    expect(wrapper.state('currentSlideIndex')).to.equal(0);
   });
 
   it('render the correct text', function () {
-    expect(wrapper.text()).toEqual('I am on slide 0');
+    expect(wrapper.text()).to.equal('I am on slide 0');
   });
 });
 
@@ -22,17 +21,15 @@ describe('<Bomb />', function () {
   const wrapper = shallow(<Bomb initialCount={120} />);
 
   it('should set the `secondsLeft` to the value of the `initialCount` prop', function () {
-    expect(wrapper.state('secondsLeft'))
-      .toNotBe(undefined, 'The `secondsLeft` property is not defined on the state.')
-      .toEqual(120);
+    expect(wrapper.state('secondsLeft')).to.equal(120);
   });
 
   it('render the correct text when the timer is not 0', function () {
-    expect(wrapper.text()).toEqual('120 seconds left before I go boom!');
+    expect(wrapper.text()).to.equal('120 seconds left before I go boom!');
   });
 
   it('render the correct text when the timer is 0', function () {
     const wrapper = shallow(<Bomb initialCount={0} />);
-    expect(wrapper.text()).toEqual('Boom!');
+    expect(wrapper.text()).to.equal('Boom!');
   });
 });
