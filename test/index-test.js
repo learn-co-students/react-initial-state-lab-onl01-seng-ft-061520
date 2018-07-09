66aa578a6cffa4000c3fae0dd7c2326a7ad0e668
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-07-09 14:18:51 -0400

    testing now

--- test/index-test.js
+++ test/index-test.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import { expect } from 'chai';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import ImageSlider from '../src/components/ImageSlider';
-import Bomb from '../src/components/Bomb';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { configure, shallow } from 'enzyme'
+import { expect } from 'chai'
+import sinon from 'sinon'
 
-Enzyme.configure({ adapter: new Adapter() })
+import Adapter from 'enzyme-adapter-react-16'
 
-describe('<ImageSlider />', function () {
-  const wrapper = shallow(<ImageSlider/>);
+import ImageSlider from '../src/ImageSlider'
+import Bomb from '../src/Bomb'
 
-  it('should set the `currentSlideIndex` to 0 on its state', function () {
+configure({ adapter: new Adapter() });
+
+describe('<ImageSlider />', () => {
+  const wrapper = shallow(<ImageSlider />);
+
+  it('should set the `currentSlideIndex` to 0 on its state', () => {
     expect(wrapper.state('currentSlideIndex')).to.equal(0);
   });
 
-  it('render the correct text', function () {
+  it('render the correct text', () => {
     expect(wrapper.text()).to.equal('I am on slide 0');
   });
 });
 
-describe('<Bomb />', function () {
+describe('<Bomb />', () => {
   const wrapper = shallow(<Bomb initialCount={120} />);
 
-  it('should set the `secondsLeft` to the value of the `initialCount` prop', function () {
+  it('should set the `secondsLeft` to the value of the `initialCount` prop', () => {
     expect(wrapper.state('secondsLeft')).to.equal(120);
   });
 
-  it('render the correct text when the timer is not 0', function () {
+  it('render the correct text when the timer is not 0', () => {
     expect(wrapper.text()).to.equal('120 seconds left before I go boom!');
   });
 
-  it('render the correct text when the timer is 0', function () {
+  it('render the correct text when the timer is 0', () => {
     const wrapper = shallow(<Bomb initialCount={0} />);
     expect(wrapper.text()).to.equal('Boom!');
   });
