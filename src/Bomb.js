ad77a4a16c14685190715bd9800990fe015c489e
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-07-09 14:20:10 -0400

    working and solution in

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
-// Bomb Component Code Goes Here
+import React from 'react';
+
+export default class Bomb extends React.Component {
+
+  constructor(props) {
+    super() 
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>{message}</div>
+    )
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,18 @@
-// Bomb Component Code Goes Here
+import React from 'react';
+
+export default class ImageSlider extends React.Component {
+
+  constructor() {
+    super() 
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    return (
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    )
+  }
+}
