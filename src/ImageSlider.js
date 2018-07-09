0db709acb0dfec32dad09bbab2fd3fead88fc62e
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-07-09 14:29:31 -0400

    gutted for master

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,18 +1,1 @@
-import React from 'react';
-
-export default class ImageSlider extends React.Component {
-
-  constructor() {
-    super() 
-
-    this.state = {
-      currentSlideIndex: 0
-    }
-  }
-
-  render() {
-    return (
-      <h1>I am on slide {this.state.currentSlideIndex}</h1>
-    )
-  }
-}
+// your ImageSlider code here!
